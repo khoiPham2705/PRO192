439abd552186b8bb5294e614bfba339570add60e
khoipham2705 <khoipham27052005@gmail.com>
2024-06-07 16:53:52 +0700

    lab5-1

---
+++ LAB5/src/lab5/pkg1/IPolymorphism.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5.pkg1;
+
+/**
+ *
+ * @author DELL
+ */
+public interface IPolymorphism {
+    int f1(String str);
+    String f2(String str);
+}
