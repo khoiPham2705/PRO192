e00b669428ce3bf0bf8977feac942bb920e4c93d
khoipham2705 <khoipham27052005@gmail.com>
2024-06-13 01:22:39 +0700

    problem

---
+++ projecteuler-net/src/problem5/NewClass.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problem5;
+
+/**
+ *
+ * @author DELL
+ */
+public class NewClass {
+    public static void main(String[] agrs){
+        int x = 1;
+        int j = 1;
+        int count=0;
+        
+        
+        while ( j == 1){
+            for (int i = 1 ; i<= 20; i++){
+                if ( x % i == 0){
+                    count++;
+                }            
+            }
+            if(count == 20){
+                System.out.println(x);
+                j = 2;
+            }
+            else{
+                count = 0;
+            }
+            x++;
+        }
+        
+        
+    }
+}
